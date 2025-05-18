5beb31f3f2063407c3b8cdd6a0ab430bb5276489
laura <laurasadxd53@gmail.com>
2025-05-17 22:42:21 -0600

    Agrega archivos principales

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req,res) => {
+res.send('¡API funcionando en Render!');
+});
+app.listen(PORT, () => {
+console.log(\'Servidor en http://localhost:${PORT}\');
+});
