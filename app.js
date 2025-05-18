48f16cbe91dab3bb159d732e84533d2ed24bd860
laura <laurasadxd53@gmail.com>
2025-05-17 23:29:52 -0600

    corrige error de sintaxis

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
 res.send('¡API funcionando en Render!');
 });
 app.listen(PORT, () => {
-console.log(\'Servidor en http://localhost:${PORT}\');
+console.log(´Servidor en http://localhost:${PORT}´);
 });
+
+
+
